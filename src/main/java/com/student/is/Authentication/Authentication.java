c517d61cccaa39fa54ad4d53a84a3c26cd08e7df
Rashid <rashid.best.ry@gmail.com>
2025-11-25 16:38:02 +0300

    auth_2

--- src/main/java/com/student/is/Authentication/Authentication.java
+++ src/main/java/com/student/is/Authentication/Authentication.java
@@ -1,11 +1,27 @@
 package com.student.is.Authentication;
 
+import java.io.*;
+
 public class Authentication {
     public static String currentUser;
     public static boolean authenticated;
 
 
     public boolean chekAuth(String login , String password) {
+
+        try  {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/com/student/is/database/auth.bin"));
+            String line;
+            while ((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
+        }
+        catch (IOException e){
+            System.out.println("Error reading file!" + e);
+        }
+
+
         String temp = DataBase_File_ogrenci(login);
         if (temp.equals(password)) {
             return true;
