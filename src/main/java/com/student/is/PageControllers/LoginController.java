ff1655fef953443538571e2ff9c106c0f32e12bf
Ali Kayli <kayliali224@gmail.com>
2025-12-02 19:30:15 +0300

    scholar studentNotes changes and update

--- src/main/java/com/student/is/PageControllers/LoginController.java
+++ src/main/java/com/student/is/PageControllers/LoginController.java
@@ -27,9 +27,9 @@ public class  LoginController {
 
     //private MediaPlayer errorPlayer;
 
-    public void StudentShowErorPopup() throws IOException {
+    public void LoginShowErorPopup() throws IOException {
         // 1. FXML dosyasını yükle
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/student/is/fxml/StudentWrongPasswordPopUp.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/student/is/fxml/LoginErrorPopUp.fxml"));
         Parent root = fxmlLoader.load();
         Stage popupStage = new Stage();
 
@@ -41,7 +41,7 @@ public class  LoginController {
     }
 
     public void ScholarShowErorPopup() throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/student/is/fxml/ScholarWrongPasswordPopUp.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/student/is/fxml/LoginErrorPopUp.fxml"));
         Parent root = fxmlLoader.load();
 
         Stage popupStage = new Stage();
@@ -125,7 +125,7 @@ public class  LoginController {
 
         } else {
             System.out.println("Hatalı öğrenci kullanıcı adı veya şifre!");
-            StudentShowErorPopup();
+            LoginShowErorPopup();
         }
 
         this.studentLogin.clear();
@@ -137,8 +137,9 @@ public class  LoginController {
     public void personelMainPage(ActionEvent event) throws IOException {
         String login = this.personelLogin.getText();
         String password = this.personelPassword.getText();
-        Boolean status = Authentication.checkPersonalAuth(login, password);
-        if (status) {
+        if (Authentication.checkPersonalAuth(login, password)) {
+            ContentLoader.setCurrentUserSession(Authentication.currentPersonalUser);
+
             Parent root = FXMLLoader.load(getClass().getResource("/com/student/is/fxml/ScholarBase.fxml"));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
@@ -146,7 +147,7 @@ public class  LoginController {
             stage.show();
         } else {
             System.out.println("Hatalı kullanıcı adı veya şifre!!");
-            ScholarShowErorPopup();
+            LoginShowErorPopup();
             //playErrorSound();
         }
         this.personelLogin.clear();
